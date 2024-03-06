0268e62fc56c135bcc54c542161c752e61021dad
fran <fgp4321@gmail.com>
2024-03-06 11:40:55 +0100

    componente add-user terminado

--- Frontend/adminCRUD/src/app/add-user/add-user.component.ts
+++ Frontend/adminCRUD/src/app/add-user/add-user.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { HttpProviderService } from "../Service/http-provider.service";
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  addUserForm: userForm = new userForm()
 
-  constructor() { }
+  @ViewChild("userForm")
+  userForm!: NgForm
+  isSubmitted: boolean = false
+  constructor(private router: Router, private httpProvider: HttpProviderService, private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  AddUser(isValid: any){
+    this.isSubmitted = true
+    if (isValid) {
+      this.httpProvider.saveUser(this.addUserForm).subscribe(async data => {
+        if (data != null && data.body != null) {
+          if (data != null && data.body != null) {
+            var resultData = data.body
+            if (resultData != null && resultData.isSuccess) {
+              this.toastr.success(resultData.message)
+              setTimeout(()=>{
+                this.router.navigate(['/Home'])
+              }, 500)
+            }
+          }
+        }
+      },
+      async error => {
+        this.toastr.error(error.message)
+        setTimeout(()=>{
+          this.router.navigate(['/Home'])
+        },500)
+      })
+    }
   }
+}
 
+
+export class userForm {
+  nombre: string = ""
+  apellido: string = ""
+  username: string = ""
+  email: string = ""
+  telefono: string = ""
 }
