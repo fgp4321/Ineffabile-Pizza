584f3a745a0e45292d6851bf1d8dc564217f23c6
fran <fgp4321@gmail.com>
2024-03-06 09:14:48 +0100

    webApi terminado para hacer llamadas GET o POST

--- Frontend/adminCRUD/src/app/app-routing.module.ts
+++ Frontend/adminCRUD/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AddUserComponent } from "./add-user/add-user.component";
+import { EditUserComponent } from "./edit-user/edit-user.component";
+import { ViewUserComponent } from "./view-user/view-user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'ViewUser/:userId', component: ViewUserComponent },
+  { path: 'AddUser', component: AddUserComponent },
+  { path: 'EditUser/:userId', component: EditUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/adminCRUD/src/app/app.module.ts
+++ Frontend/adminCRUD/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddUserComponent,
+    EditUserComponent,
+    ViewUserComponent
   ],
   imports: [
     BrowserModule,
