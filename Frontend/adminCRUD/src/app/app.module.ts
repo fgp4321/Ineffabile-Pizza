907ae230468946c8e04180819212635ee7b53de8
fran <fgp4321@gmail.com>
2024-03-06 10:30:46 +0100

    home.html hecho, estilos .scss, iconos y estilos de fontawesome y bootstrap y cambios en app.module.ts

--- Frontend/adminCRUD/src/app/app.module.ts
+++ Frontend/adminCRUD/src/app/app.module.ts
@@ -4,21 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
-import { ViewUserComponent } from './view-user/view-user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ViewUserComponent,
     AddUserComponent,
-    EditUserComponent,
-    ViewUserComponent
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
