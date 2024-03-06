eea2f104817cc59acdb4954fd272fc9ffce52a93
fran <fgp4321@gmail.com>
2024-03-06 12:40:35 +0100

    componente de edit-user terminado

--- Frontend/adminCRUD/src/app/edit-user/edit-user.component.ts
+++ Frontend/adminCRUD/src/app/edit-user/edit-user.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { HttpProviderService } from "../Service/http-provider.service";
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.scss']
 })
-export class EditUserComponent implements OnInit {
 
-  constructor() { }
+export class EditUserComponent implements OnInit {
+  editUserForm: userForm = new userForm()
+
+  @ViewChild("userForm")
+  userForm!: NgForm
+
+  isSubmitted: boolean = false
+  userId: any
+
+  constructor(private toastr: ToastrService, private route: ActivatedRoute, private router: Router, private httpProvider: HttpProviderService) { }
 
   ngOnInit(): void {
+    this.userId = this.route.snapshot.params["userId"]
+    this.getUserDetailByID()
   }
 
+  getUserDetailByID(){
+    this.httpProvider.getUserDetailByID(this.userId).subscribe((data: any)=>{
+      if (data != null && data.body != null) {
+        var resultData = data.body
+        if (resultData) {
+          this.editUserForm.nombre = resultData.nombre;
+          this.editUserForm.apellido = resultData.apellido;
+          this.editUserForm.username = resultData.username;
+          this.editUserForm.email = resultData.email;
+          this.editUserForm.telefono = resultData.telefono;
+        }
+      }
+    },
+    (error:any) => { })
+  }
+
+  EditUser(isValid: any){
+    this.isSubmitted = true
+    if (isValid) {
+      this.httpProvider.saveUser(this.editUserForm).subscribe(async data =>{
+        if (data != null && data.body != null) {
+          var resultData = data.body
+          if (resultData != null && resultData.isSuccess) {
+            if (resultData != null && resultData.isSuccess) {
+              this.toastr.success(resultData.message)
+              setTimeout(()=>{
+                this.router.navigate(['/Home'])
+              },500)
+            }
+          }
+        }
+      },
+      async error => {
+        this.toastr.error(error.message)
+        setTimeout(()=>{
+          this.router.navigate(['/Home'])
+        },500)
+      }
+      )
+    }
+  }
+}
+
+export class userForm {
+  nombre: string = ""
+  apellido: string = ""
+  username: string = ""
+  email: string = ""
+  telefono: string = ""
 }
