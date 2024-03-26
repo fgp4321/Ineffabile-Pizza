f7b3cee1c693f7f16cd5ee55dd7b4ec8f9d29493
Fran Gonzalez <fgonzalez@delfingrupo.com>
2024-03-26 10:37:50 +0100

    cambios en los estilos y continuacion de carrito

--- Backend/index.js
+++ Backend/index.js
@@ -256,8 +256,9 @@ app.get('/promociones', async (req, res) => {
 
 // Ruta para agregar productos al carrito
 app.post('/add-to-cart', (req, res) => {
-    const { id, name, price_pvp, quantity } = req.body; // Cambia price a price_pvp
+    const { id, name, price_pvp, quantity, categoria_nombre, imagen1 } = req.body;
     
+    console.log('Datos recibidos:', req.body);
     // Obtén el carrito de la sesión
     let cart = req.session.cart || [];
     
@@ -265,8 +266,10 @@ app.post('/add-to-cart', (req, res) => {
     cart.push({
       id: id,
       name: name,
-      price: price_pvp, // Cambia price a price_pvp
-      quantity: quantity
+      price_pvp: price_pvp,
+      quantity: quantity,
+      categoria_nombre: categoria_nombre,
+      imagen1: imagen1
     });
   
     // Guarda el carrito en la sesión
@@ -276,17 +279,17 @@ app.post('/add-to-cart', (req, res) => {
     res.status(200).send('Producto agregado al carrito');
 });
 
-  app.post('/eliminar-producto', (req, res) => {
+app.post('/eliminar-producto', (req, res) => {
     const productId = req.body.id;
     // Elimina el producto del carrito (implementa la lógica según tu aplicación)
     // Por ejemplo:
     req.session.cart = req.session.cart.filter(item => item.id !== productId);
     // Retorna una respuesta adecuada, como un código de estado 200 para indicar éxito
     res.sendStatus(200);
-  });
+});
   
   // Ruta para mostrar el carrito
-  app.get('/carrito', (req, res) => {
+app.get('/carrito', (req, res) => {
     // Obtén el carrito de la sesión
     const cart = req.session.cart || [];
     
@@ -297,7 +300,7 @@ app.post('/add-to-cart', (req, res) => {
         }
         acc[product.id].quantity += 1;
         return acc;
-    }, {});
+}, {});
 
     // Convierte el objeto agrupado en un array para pasarlo a la vista
     const cartItems = Object.values(groupedCart);
