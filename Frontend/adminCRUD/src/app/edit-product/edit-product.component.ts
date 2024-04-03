b14207377371457ddb2474ab6031a03ae25a2f29
Fran Gonzalez <fgonzalez@delfingrupo.com>
2024-04-03 09:39:14 +0200

    CRUD de productos al 90%

--- Frontend/adminCRUD/src/app/edit-product/edit-product.component.ts
+++ Frontend/adminCRUD/src/app/edit-product/edit-product.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { HttpProviderService } from "../Service/http-provider.service";
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +11,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  editProductForm: productForm = new productForm()
+
+  @ViewChild("productForm")
+  productForm!: NgForm
+
+  isSubmitted: boolean = false
+  productId: any
+
+  constructor(private toastr: ToastrService, private route: ActivatedRoute, private router: Router, private httpProvider: HttpProviderService) { }
 
   ngOnInit(): void {
+    this.productId = this.route.snapshot.params["productId"]
+    this.getProductDetailByID()
   }
 
+  getProductDetailByID(){
+    this.httpProvider.getProductDetailByID(this.productId).subscribe((data: any)=>{
+      if (data != null && data.body != null) {
+        var resultData = data.body
+        if (resultData) {
+          this.editProductForm.nombre = resultData.nombre;
+          this.editProductForm.descripcion = resultData.descripcion;
+          this.editProductForm.precio_pvp = resultData.precio_pvp;
+          this.editProductForm.precio_oferta = resultData.precio_oferta;
+          this.editProductForm.categoria_nombre = resultData.categoria_nombre;
+          this.editProductForm.imagen1 = resultData.imagen1;
+        }
+      }
+    },
+    (error:any) => { })
+  }
+
+  EditProduct(isValid: any) {
+    this.isSubmitted = true;
+  
+    if (isValid) {
+      this.httpProvider.editProduct(this.productId, this.editProductForm).subscribe(
+        async (data) => {
+          if (data != null && data.body != null) {
+            var resultData = data.body;
+            this.toastr.success(`Producto: "${resultData.nombre}" modificado correctamente.`);
+            setTimeout(() => {
+              this.router.navigate(['/Home']);
+            }, 500);
+          }
+        },
+        async (error) => {
+          this.toastr.error(error.message);
+          setTimeout(() => {
+            this.router.navigate(['/Home']);
+          }, 500);
+        }
+      );
+    }
+  }
+}
+
+export class productForm {
+  nombre: string = "";
+  descripcion: string = "";
+  precio_pvp: string = "";
+  precio_oferta: string = "";
+  categoria_nombre: string = "";
+  imagen1: string = "";
 }
--- Frontend/adminCRUD/src/app/view-product/view-product.component.ts
+++ Frontend/adminCRUD/src/app/view-product/view-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpProviderService } from "../Service/http-provider.service";
+import { WebApiService } from "../Service/web-api.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-view-product',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProductComponent implements OnInit {
 
-  constructor() { }
+  productId: any
+  productDetail: any = []
+  showPassword: boolean = false;
+
+  constructor(private toastr: ToastrService,public webApiService: WebApiService, private route: ActivatedRoute, private httpProvider: HttpProviderService) { }
 
   ngOnInit(): void {
+    this.productId = this.route.snapshot.params['productId']
+    this.getProductDetailByID();
+  }
+
+  getProductDetailByID(){
+    this.httpProvider.getProductDetailByID(this.productId).subscribe(
+      (data: any) => {
+        if (data != null && data.body != null) {
+          var resultData = data.body;
+          if (resultData) {
+            this.toastr.success(`Visualizando detalles de ${resultData.nombre}.`);
+            this.productDetail = resultData;
+          }
+        }
+      },
+      (error: any) => {
+        console.error('Error obteniendo detalles del usuario:', error);
+      }
+    );
   }
 
 }
