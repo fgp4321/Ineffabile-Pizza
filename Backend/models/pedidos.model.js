bf359e4f500af0c6e2cc789428aea4614e7867c7
Fran Gonzalez <fgonzalez@delfingrupo.com>
2024-04-24 09:03:59 +0200

    operaciones modelo y controlador de pedidos

--- Backend/models/pedidos.model.js
+++ Backend/models/pedidos.model.js
@@ -27,5 +27,61 @@ const pedidosSchema = new mongoose.Schema({
 
 const Pedido = mongoose.model("Pedido", pedidosSchema)
 
+Pedido.findPedidos = async function(){
+    try {
+        const pedidos = await Pedido.find()
+        return pedidos
+    } catch (error) {
+        throw error;
+    }
+}
+
+Pedido.buscarPorId = async function (pedidoId) {
+    try {
+        const pedido = await Pedido.findById(pedidoId)
+        if (pedido) {
+            return pedido
+        } else {
+            return null
+        }
+    } catch (error) {
+        throw error
+    }
+};
+
+Pedido.crearPedido = async (nuevoPedido) => {
+    try {
+        const pedidoCreado = await Pedido.create(nuevoPedido)
+        return nuevoPedido;
+    } catch (error) {
+        throw error
+    }
+};
+
+Pedido.actualizarPedido = async function (pedidoId, datosActualizados) {
+    try {
+        const pedidoActualizado = await Pedido.findByIdAndUpdate(pedidoId, datosActualizados, { new: true })
+        if (pedidoActualizado) {
+            return pedidoActualizado
+        } else {
+            return null
+        }
+    } catch (error) {
+        throw error
+    }
+};
+
+Pedido.eliminarPedido = async function (pedidoId) {
+    try {
+        const pedidoEliminado = await Pedido.findByIdAndDelete(pedidoId)
+        if (pedidoEliminado) {
+            return pedidoEliminado
+        } else {
+            return null
+        }
+    } catch (error) {
+        throw error
+    }
+}
 
 module.exports = Pedido
