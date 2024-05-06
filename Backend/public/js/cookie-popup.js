a94126fb09976d4745cfc0417320a17e330cd9e6
Fran Gonzalez <fgonzalez@delfingrupo.com>
2024-05-06 09:32:19 +0200

    nuevo sistema de cookies

--- Backend/public/js/cookie-popup.js
+++ Backend/public/js/cookie-popup.js
@@ -2,36 +2,44 @@ document.addEventListener("DOMContentLoaded", function() {
     var cookiePopup = document.getElementById("cookie-popup");
     var acceptButton = document.getElementById("accept-cookies");
     var rejectButton = document.getElementById("reject-cookies");
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    // Mostrar el pop-up solo si la cookie 'cookies-accepted' no está establecida
-    if (!getCookie("cookies-accepted")) {
+    // Mostrar el pop-up solo si la cookie 'functional-cookie' no está establecida como 'true'
+    if (getCookie("functional-cookie") !== "true") {
         cookiePopup.style.display = "block";
     }
 
-    // Manejar clic en el botón 'Aceptar'
     acceptButton.addEventListener("click", function() {
-        setCookie("cookies-accepted", "true", 365); // Establecer la cookie por un año
+        // Establecer las cookies según las selecciones del usuario
+        checkboxes.forEach(function(checkbox) {
+            setCookie(checkbox.name + "-cookie", checkbox.checked ? "true" : "false", 365);
+        });
         cookiePopup.style.display = "none";
     });
 
-    // Manejar clic en el botón 'No aceptar'
     rejectButton.addEventListener("click", function() {
+        // Establecer todas las cookies como 'false' excepto las funcionales que deben ser 'true'
+        checkboxes.forEach(function(checkbox) {
+            if (checkbox.name === "functional") {
+                setCookie("functional-cookie", "true", 365); // La cookie funcional no se puede rechazar
+            } else {
+                setCookie(checkbox.name + "-cookie", "false", 365);
+            }
+        });
         cookiePopup.style.display = "none";
     });
 
-    // Función para obtener el valor de una cookie
     function getCookie(name) {
         var nameEQ = name + "=";
-        var cookies = document.cookie.split(';');
-        for(var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i];
-            while (cookie.charAt(0) === ' ') cookie = cookie.substring(1, cookie.length);
-            if (cookie.indexOf(nameEQ) === 0) return cookie.substring(nameEQ.length, cookie.length);
+        var ca = document.cookie.split(';');
+        for(var i=0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
         }
         return null;
     }
 
-    // Función para establecer una cookie
     function setCookie(name, value, days) {
         var expires = "";
         if (days) {
